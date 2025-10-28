f3990734e429c831be115b9a418f192a1209dd42
Delfina Brenda Vega <delfinabrenda06@gmail.com>
2025-10-28 10:39:46 -0300

    Se agregaron rutas para la navegacion entre paginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Login } from './Login'
+import { Dashboard } from './Dashboard'
 import { Header } from './Header'
 
 function App() {
   return (
-    <>
-      <Header />
-      <Login />
-    </>
+    <BrowserRouter>
+    <Routes>
+      {/*Ruta del Login (Pagina inicial) */}
+      <Route path="/" element={
+      <>
+        <Header />
+        <Login />
+      </>
+     } />
+
+     {/*Ruta del Dashboard */}
+     <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
