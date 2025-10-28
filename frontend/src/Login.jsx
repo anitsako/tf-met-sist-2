7813e72a926e7a4d28964b6523e55ad608e47016
Delfina Brenda Vega <delfinabrenda06@gmail.com>
2025-10-28 10:46:35 -0300

    Se uso Navigate para redirigir al Dashboard al iniciar sesion

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from 'react-router-dom'
 import './login.css'
+
 // #51acea color para calendario de turnos
 // #d3d5d4 color para letras claras
 // #ebeff8 color para hero dashboard
 
 export function Login() {
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        //Por ahora navega directo al dashboard (sin validar)
+        navigate('/dashboard')
+    }
     return (
         <div>
-            <form className='login-form' action="">
+            <form className='login-form' onSubmit={handleSubmit}>
                 <h2 className="login-title">Accede a tu cuenta</h2>
                 <input type="text" id="username" name="username" placeholder='usuario'/>
                 <br />
