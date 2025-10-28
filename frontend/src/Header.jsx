38e087d0c0f8cef334a706fc7c9e18c7ef9dde98
Delfina Brenda Vega <delfinabrenda06@gmail.com>
2025-10-28 10:20:39 -0300

    Se añadio una version mas compacta para el Dashboard

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,9 +1,10 @@
 import './index.css'
 
-export function Header() {
+//Se agrego una propiedad (compact) para quitar el espacio entre el header y la pagina solo en el Dashboard
+export function Header({ compact = false }) {
     
     return (
-        <header className="header">
+        <header className={`header ${compact ? 'header-compact' : ''}`}>
             <img id='logo-clinica' src="/logo-clinica.svg" alt="logo de la clinica" /> <h1 id='title'>CLINICA <span style={{color: '#0780df'}}>
                 SALUD</span></h1>
         </header>
